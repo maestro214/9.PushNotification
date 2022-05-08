6eeb05b97af7adb7606dc6d99dd7fed6a0b0b119
Jung <maestro214@naver.com>
2022-05-08 21:38:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pushnotification/NotificationType.kt
@@ -0,0 +1,9 @@
+package com.example.pushnotification
+
+enum class NotificationType(val title: String, val id: Int) {
+
+    NORMAL("일반 알림", 0),
+    EXPANDABLE("확장형 알림",1),
+    CUSTOM("커스텀 알림",3)
+
+}
